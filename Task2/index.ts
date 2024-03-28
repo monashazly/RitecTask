07576a38a3c081f8a7d3df3a8a96edd67c5c2ca6
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-03-29 00:07:35 +0200

    convert for loop to for loop of elements

--- Task2/index.ts
+++ Task2/index.ts
@@ -1,19 +1,17 @@
 
-const names:string[] = [ 'Mona' , 'Shazly', 'Alice', 'Bob', 'mona', 'Charlie', 'David', 'Eve']
-const iterations:number = names.length
+const names:string[] = [ 'Mona' , 'shazly', 'tarek', 'walid', 'mona', 'nour', 'noha', 'kareem' , 'amir']
+const lowerCaseNames = names.map(name => name.toLowerCase());
 const seenNames:string[] = []
+const sortedNames:string[] = lowerCaseNames.sort()
 
-for(let i:number = 0 ; i < iterations ; i++){
-    const name = names[i] 
 
-    if(name.length > 10) break ;
-
-    if( name.toLocaleLowerCase().includes('a') && !seenNames.includes(name)){
-        
-        console.log(name)
-    } 
+for (const name of sortedNames){
     
-    seenNames.push(name.toLocaleLowerCase())
+    if(name.length > 255) break;
+
+    if( name.includes('a') && !seenNames.includes(name) ) console.log(name)
+    
+    seenNames.push(name)
 
 }
 
