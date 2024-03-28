221fe667c43183eb859215cd7ef65138f8189abb
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-03-29 01:34:29 +0200

    rounding

--- task3/index.ts
+++ task3/index.ts
@@ -11,22 +11,31 @@ function calculateTotals(discount:number , items:Item[] ):{
    SST:number,
    totalService:number
 }{
-    let totalBeforeTax = 0;
-    let totalDiscount = 0;
+    let totalBeforeTax = 0
+    let totalOfTaxable = 0 
+    let totalOfNonTaxable = 0 
+    let totalDiscount = 0
     let totalAfterDiscount = 0;
-    items.forEach((item:Item)=>{
-        totalBeforeTax = totalBeforeTax + item.price 
-    })
+    let SST = 0
+    let totalService = 0
 
+    items.forEach((item:Item)=>{
+        if(item.taxable) totalOfTaxable = totalOfTaxable + item.price 
+        if(!item.taxable) totalOfNonTaxable = totalOfNonTaxable + item.price
+    })
+     
+    totalBeforeTax = totalOfTaxable + totalOfNonTaxable
     totalDiscount = (totalBeforeTax * discount)/100
     totalAfterDiscount = totalBeforeTax - totalDiscount
+    SST = totalOfTaxable * 0.06
+    totalService = totalOfTaxable * 0.1
 
     return {
-       totalBeforeTax,
-       totalDiscount,
-       totalAfterDiscount,
-       SST:55,
-       totalService:55
+       totalBeforeTax : Math.round(totalBeforeTax),
+       totalDiscount : Math.round(totalDiscount),
+       totalAfterDiscount : Math.round(totalAfterDiscount),
+       SST : Math.round(SST),
+       totalService: Math.round(totalService)
     }
 }
 
